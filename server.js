21896852b6cf109ca986499e55cadb24fe1ce950
Ashwin M <ashwin18999m@gmail.com>
2025-06-25 20:05:51 +0530

    Update CORS config for Vercel frontend

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",                      
+    "https://linktree-frontend-one.vercel.app/"  
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
